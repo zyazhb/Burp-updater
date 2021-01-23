e795f06f0360ffda1a139a95b69b83b39d362f7f
ZYA <zyazhb@yahoo.com>
2021-01-23 10:42:33 +0800

    windows ver

--- main.go
+++ main.go
@@ -16,9 +16,9 @@ func main() {
 	var wg sync.WaitGroup
 
 	api := "https://portswigger.net/burp/releases/download?product=pro&type=windowsx64&version="
-	fmt.Printf("\033[1;34m A Burpsuite Update Finder BY-ZYA\n \033[0m")
-	fmt.Printf("\033[1;34m   _____             _____     _ _          _____       _     _          _____ _       _         \n   | __  |_ _ ___ ___|   __|_ _|_| |_ ___   |  |  |___ _| |___| |_ ___   |   __|_|___ _| |___ ___ \n   | __ -| | |  _| . |__   | | | |  _| -_|  |  |  | . | . | .'|  _| -_|  |   __| |   | . | -_|  _|\n   |_____|___|_| |  _|_____|___|_|_| |___|  |_____|  _|___|__,|_| |___|  |__|  |_|_|_|___|___|_|  \n				 |_|                              |_|                                             \n\033[0m")
-	fmt.Printf("\033[1;30m [-]Finding...\n \033[0m")
+	fmt.Printf("A Burpsuite Update Finder BY-ZYA\n ")
+	fmt.Printf("   _____             _____     _ _          _____       _     _          _____ _       _         \n   | __  |_ _ ___ ___|   __|_ _|_| |_ ___   |  |  |___ _| |___| |_ ___   |   __|_|___ _| |___ ___ \n   | __ -| | |  _| . |__   | | | |  _| -_|  |  |  | . | . | .'|  _| -_|  |   __| |   | . | -_|  _|\n   |_____|___|_| |  _|_____|___|_|_| |___|  |_____|  _|___|__,|_| |___|  |__|  |_|_|_|___|___|_|  \n				 |_|                              |_|                                             \n")
+	fmt.Printf("[-]Finding...\n")
 
 	for y := 2020; y <= time.Now().Year(); y++ {
 		for m := 1; m <= 12; m++ {
@@ -31,7 +31,7 @@ func main() {
 		}
 	}
 	wg.Wait()
-	fmt.Printf("\033[1;34m [+]Done! \033[0m")
+	fmt.Printf("[+]Done!")
 	time.Sleep(1 * time.Minute)
 	os.Exit(0)
 }
@@ -52,7 +52,7 @@ func rescode(api string, ver string, wg *sync.WaitGroup) {
 
 	_, err = ioutil.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Println("\033[1;32m [+]Found a version! 033[0m", api+ver)
+		fmt.Println("[+]Found a version!", api+ver)
 		return
 	}
 	resp.Body.Close()
