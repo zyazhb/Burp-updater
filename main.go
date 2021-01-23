7ad42cf44ba16cb349b515121db8ecae4ffc86ea
ZYA <zyazhb@yahoo.com>
2021-01-23 10:10:07 +0800

    remove comment

--- main.go
+++ main.go
@@ -15,14 +15,11 @@ import (
 func main() {
 	var wg sync.WaitGroup
 
-	// api := "https://portswigger.net/burp/releases/download?product=pro&version=2020.11.3&type=jar"
 	api := "https://portswigger.net/burp/releases/download?product=pro&type=windowsx64&version="
 	for y := 2020; y <= time.Now().Year(); y++ {
 		for m := 1; m <= 12; m++ {
 			for d := 1; d <= 3; d++ {
 				ver := fmt.Sprint(y) + "." + fmt.Sprint(m) + "." + fmt.Sprint(d)
-				// apiurl = api + "2020.11.3"
-				// fmt.Printf("testing %s\n", api)
 				wg.Add(1)
 				go rescode(api, ver, &wg)
 
@@ -49,12 +46,9 @@ func rescode(api string, ver string, wg *sync.WaitGroup) {
 
 	_, err = ioutil.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Println("Found a version!", api, ver)
-		// fmt.Println("ioutil.ReadAll err=", err)
+		fmt.Println("Found a version!", api+ver)
 		return
 	}
-	// fmt.Println(len(bytes))
 	resp.Body.Close()
-	// fmt.Printf("%s,%d\r\n", apiurl, resCode)
 	return
 }
